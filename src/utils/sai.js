dc81a5ecae1baab538abc45be8e527c94f2f9631
saisumanth1111 <arabusai4024@gmail.com>
2024-10-05 20:46:11 +0530

    jira-2

--- src/utils/sai.js
+++ src/utils/sai.js
@@ -1,13 +1,7 @@
-const { MdTrackChanges } = require("react-icons/md");
-
-this is the feature version with latest MdTrackChanges
-
-import React from 'react'
+import React from "react";
 
 const sai = () => {
-  return (
-    <div>sai</div>
-  )
-}
+  return <div>sai</div>;
+};
 
-export default sai
+export default sai;
